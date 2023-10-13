961908b4ca3b505b975595aa68c42be07d7b0c8c
KalyanGundu <kalyanraj567@yahoo.com>
2023-10-13 15:18:33 +0530

    WriteToDB

--- DataExtraction/WriteToDB.py
+++ DataExtraction/WriteToDB.py
@@ -1,9 +1,6 @@
 import mysql.connector
 
-host = "162.241.123.123"
-username = "classle3"
-password = "0p=HJ^q~$pT-"
-database = "classle3_mns_credit"
+
 
 db_connection = mysql.connector.connect(
     host=host,
